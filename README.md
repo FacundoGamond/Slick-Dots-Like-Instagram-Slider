a5b2aaf712df23048aa034c0585f84c802098080
Facundo Gamond <facugamond@hotmail.com>
2021-02-06 22:00:09 -0300

    readme done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Slick-Dots-Like-Instagram-Slider
+A solution to have instagram dots in a slick slider.
+
+## Usage
+Add to your head jquery, slick, slick-custom-dots with their respective css files as in the example of the index.html file
+
+### JS Custom Functon
+Call the function slick Slider (slider, settings), it accepts two parameters: slider and settings.
+
+#### Example
+var slider = $('.slick-slider');
+var settings = {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true, // IMPORTANT
+        focusOnSelect: true,
+        infinite: false, // IMPORTANT
+    }
+
+slickSlider(slide, settings);
--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,37 +1,7 @@
 $(document).ready(function(){
 
-    //LOAD INSTAGRAM PHOTOS(AJAX) WITH SLICK
-    var igToken = "IGQVJXenlOTFFMSThrNzBSUjJhb1dyY0xFcE1jdTVZATTRoTnYtOUNnUGhuYUlkbkYwSUtQMEhBU0psUXRVVHhNQzB4Q2FhVnFQRmpRbG9aZAXd1MTZAlUXIwa1gwaTBmamdwWjVqWmtOV0NadVZASVlU3bQZDZD"; //YOUR INSTAGRAM TOKEN GOES HERE
-
-    $.ajax({
-        type: 'get',
-        url: "https://graph.instagram.com/me/media?access_token="+igToken+"&fields=media_url,media_type,caption,permalink",
-        success: function (response) {
-            response.data.forEach((element, index) => {
-                if (response.data[index].media_type != "VIDEO") {
-                    $('.js-slick-slider').append(`
-                        <div class="slide" style="background-image: url(' `+ response.data[index].media_url + `')">
-                            <h3>`+ response.data[index].caption + `</h3>
-                        </div>`);
-                }
-            });
-    
-            //SLICK CALL
-            slickSlider($('.js-slick-slider'), {
-                arrows: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                focusOnSelect: true,
-                infinite: false,
-                centerMode: true
-            });
-        }
-    });
-
-
-    //SLICK CALL
-    slickSlider($('.slick-slider-instagram-dots'), {
+    //SLICK CALL (slide, slick json settings)
+    slickSlider($('.slick-slider'), {
         arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -39,5 +9,6 @@ $(document).ready(function(){
         focusOnSelect: true,
         infinite: false,
     });
-})
+
+});
 
